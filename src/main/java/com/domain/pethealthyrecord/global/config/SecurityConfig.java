49c7cf8b87f7678e2a8e6614a56d0d93da4472c3
mmy428 <mmy428@naver.com>
2026-06-10 23:24:44 +0900

    0610

---
+++ src/main/java/com/domain/pethealthyrecord/global/config/SecurityConfig.java
@@ -0,0 +1,35 @@
+package com.domain.pethealthyrecord.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(csrf -> csrf.disable())
+
+                .headers(headers -> headers
+                        .frameOptions(frame -> frame.disable())
+                )
+
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                "/swagger-ui/**",
+                                "/v3/api-docs/**",
+                                "/h2-console/**"
+                        ).permitAll()
+
+                        .anyRequest().permitAll()
+                );
+
+        return http.build();
+    }
+}
